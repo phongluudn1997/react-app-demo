548df8a186cf8c72e9e766fee1693474827427f0
Phong Luu <huynhpluu@gmail.com>
2020-11-30 21:11:45 +0700

    completed: auth-context

--- src/context/auth-context.js
+++ src/context/auth-context.js
@@ -2,6 +2,7 @@ import * as React from "react"
 
 import { useAsync } from "../utils/hooks/useAsync"
 import * as auth from "../providers/auth-provider"
+import { client } from "../utils/api-clients"
 
 const AuthContext = React.createContext()
 AuthContext.displayName = "AuthContext"
@@ -20,11 +21,14 @@ const AuthProvider = props => {
     isLoading,
     isSuccess,
     isError,
-    status,
     error,
     setData,
     execute,
   } = useAsync()
+
+  React.useEffect(() => {
+    execute(client("me", { token: localStorage.getItem("token") }))
+  }, [execute])
 
   const login = React.useCallback(
     form => {
@@ -38,15 +42,27 @@ const AuthProvider = props => {
     [setData]
   )
 
+  const logout = React.useCallback(() => {
+    auth.logout()
+    setData(null)
+  }, [setData])
+
   const value = React.useMemo(
     () => ({
       login,
       register,
+      user,
+      logout,
     }),
-    [login, register]
+    [login, logout, register, user]
   )
 
-  return <AuthContext.Provider value={value} {...props} />
+  if (isLoading) return <h1>Loading...</h1>
+
+  if (isError) return <h1>Error: {error.message}</h1>
+
+  if (isSuccess) return <AuthContext.Provider value={value} {...props} />
+  else throw new Error("Unhandle status")
 }
 
 export { useAuth, AuthProvider }
